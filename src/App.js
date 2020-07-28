7e1fffd217addf569f57ef5a9a67a50cb2d590e8
fmwong77 <fongmeiw@gmail.com>
2020-07-28 14:18:29 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import EmployeesList from './components/EmployeesList';
+import EmployeesList from './components/EmployeesListWithReactHooks';
 
 class App extends Component {
-  render() {
-    const { employees } = this.props;
-    return (
-      <div className="App">
-        <EmployeesList employees={employees} />
-      </div>
-    );
-  }
+	render() {
+		const { employees } = this.props;
+		return (
+			<div className="App">
+				<EmployeesList employees={employees} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,22 +1,44 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			employees: this.props.employees,
+			filterBy: '',
+		};
+	}
 
-  render() {
-    const { employees } = this.props;
-    return (
-      <React.Fragment>
-        <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
-        </div>
-        <ul className="employees-list">
-          { employees.map(employee => (
-            <li key={employee.name} data-testid="employee">{employee.name}</li>
-          ))}
-        </ul>
-      </React.Fragment>
-    );
-  }
+	handleChange = (e) => {
+		this.setState({ filterBy: e.target.value.toLowerCase() });
+	};
+
+	render() {
+		// const { employees } = this.props;
+		return (
+			<React.Fragment>
+				<div className="controls">
+					<input
+						type="text"
+						className="filter-input"
+						data-testid="filter-input"
+						onChange={this.handleChange}
+					/>
+				</div>
+				<ul className="employees-list">
+					{this.state.employees.map((employee) => {
+						if (employee.name.toLowerCase().includes(this.state.filterBy)) {
+							return (
+								<li key={employee.name} data-testid="employee">
+									{employee.name}
+								</li>
+							);
+						}
+					})}
+				</ul>
+			</React.Fragment>
+		);
+	}
 }
 
 export default EmployeesList;
